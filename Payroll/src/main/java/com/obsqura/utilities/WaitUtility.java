b4774934a8defd83e000b7caf7ca664a65fe9668
nimmi <nimmi@DESKTOP-P284K5H>
2023-07-20 05:48:02 -0700

    Commit with wait utility changes

--- Payroll/src/main/java/com/obsqura/utilities/WaitUtility.java
+++ Payroll/src/main/java/com/obsqura/utilities/WaitUtility.java
@@ -11,16 +11,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WaitUtility {
 	WebDriver driver;
-	
+
 	public WaitUtility(WebDriver driver) {
 		this.driver = driver;
-		
+
 	}
-	
-	  public void waitforelementtobepresent(WebElement element) {
-	  WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(100));
-	  wait.until(ExpectedConditions.visibilityOf(element));
-	  }
-	 
+
+	public void waitforelementtobepresent(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public void IW_implicitwat(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	public void Elementstobeclickable(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+	}
 
 }
